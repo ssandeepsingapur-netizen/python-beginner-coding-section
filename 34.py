616825d28017cb7f209d1e79c99b9a3e0e5020ed
sandeep-tech <ssandeepsingapur@gmail.com>
2026-05-03 09:38:28 -0700

    Add files via upload

---
+++ 34.py
@@ -0,0 +1,7 @@
+def full_name(first_name, last_name):
+    result=(first_name+" "+last_name)
+    print(result)
+
+first_name = input("enter first your name")
+last_name = input("enter second name")
+full_name(first_name,last_name)
