c3b528d501a41e4e9f223221c83bf4ff35a670d8
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-08 09:51:25 -0700

    Add files via upload

--- 26.py
+++ 26.py
@@ -1,9 +1,24 @@
-class student:
-    def __init__ (self, name, age):
-        self.name = name
-        self.age = age
-    def display(self):
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-s1 = student("sandy", 19)
-s1.display()
+
+file = open("demo.txt", "w")
+file.write("hello sndy how are you?")
+file.close()
+
+
+file = open("demo.txt", "r")
+data = file.read()
+file.close()
+print(data)
+
+file = open("demo.txt", "a")
+file.write(" I am fine.")
+file.close()
+
+file = open("demo.txt", "r")
+
+data = file.read()
+
+new_data = data.replace(" ", "")
+
+print(new_data)
+
+file.close()
