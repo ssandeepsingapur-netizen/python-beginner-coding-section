183064d3d39489c5bf63de8784bf4fb8711ef2f8
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-08 09:51:43 -0700

    Add files via upload

--- 27.py
+++ 27.py
@@ -1,6 +1,19 @@
-class calculator:
-    def add(self, a, b):
-        return a + b
-    
-c = calculator()
-print(c.add(5, 3))
+import math
+
+num = 25
+
+print("Square Root:", math.sqrt(num))
+print("Power:", math.pow(2, 3))
+print("Factorial:", math.factorial(5))
+
+
+import random
+print("Random Numbers between 1 and 10:", random.randint(1, 10))
+print("Random Float between 0 and 1:", random.random())
+
+import calendar
+
+year = 2026
+month = 6
+
+print(calendar.month(year, month))
