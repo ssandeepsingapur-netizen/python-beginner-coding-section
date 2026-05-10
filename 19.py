dd1ee9bc9fe620ca139f5a8ca2ab7a61a73b7aab
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-10 10:07:35 -0700

    Add files via upload

--- 19.py
+++ 19.py
@@ -1,10 +1,32 @@
-def check_num(n):
-    if n > 0:
-        print("positive")
-    elif n < 0:
-        print("negative")
-    else:
-        print("zero")
-check_num(5)
-check_num(-3)
-check_num(0)
+import math
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def multiple(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+
+num = int(input("enter number"))
+a = int(input("enter first number"))
+b =int(input("enter second number"))
+
+
+if num == 1:
+    result = add(a,b)
+    print(result)
+elif num ==2:
+    result = sub(a,b)
+    print(result)
+elif num == 3:
+    result = multiple(a,b)
+    print(result)
+
+elif num == 4:
+    try:
+        result =  divide(a,b)
+        print(result)
+    except ZeroDivisionError:
+        print("we cannot divide by using zero")
