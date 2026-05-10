dcda7c16c04fa49a09bc013ea6a635b7cfbe58fa
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-10 10:07:20 -0700

    Add files via upload

--- 18.py
+++ 18.py
@@ -1,9 +1,6 @@
-def is_disarium(num):
-    num_str = str(num)
-    digit_sum = sum((i) ** (index +1) for index, i in enumerate(num))
-    return num == digit_sum
-
-disarium_numbers = [num for num in range(1, 101) if is_disarium(num)]
-print("disarium numbers between 1 and 100:")
-for num in disarium_numbers:
-    print(num, end=" | ")
+try:
+    file = open("sample.txt","r+")
+    data = file.read()
+    print(data)
+except:
+    print("file not found")
