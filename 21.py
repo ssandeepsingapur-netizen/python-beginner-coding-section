8065520402b6913273cdd55b112c74dd267399e9
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-11 09:46:46 -0700

    Add files via upload

--- 21.py
+++ 21.py
@@ -1,11 +1,4 @@
-def find_largest(a, b, c):
-    if a >= b and a>= c:
-        return a
-    elif b >= a and b >= c:
-        return b
-    else:
-        return c
-num1 = 10
-num2 = 25
-num3 = 15
-print(f"The largest number among {num1}, {num2}, and {num3} is: {find_largest(num1, num2, num3)}")
+N =int(input())
+
+for i in range(N):
+    print(i*10**i//9)
