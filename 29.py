0dbb34636431d228aea035f60fe4883416f14f0e
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-16 08:16:59 -0700

    Add files via upload

--- 29.py
+++ 29.py
@@ -1,10 +1,8 @@
-class cat:
-    def sound(self):
-        print("Meow")
+row = 5
 
-class cow:
-    def sound(self):
-        print("amboo")
-
-for animal in (cat(),cow()):
-    animal.sound()
+for i in range(row):
+    for j in range(row - i - 1):#5-0-1,5-1-1,5-2-1,5-3-1,5-4-1
+        print(" ", end= " ")
+    for k in range(2 * i + 1):#1,3,5,7,9
+        print("*", end= " ")
+    print()
