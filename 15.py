b9cee2c74dbd49e2bcffa0283bb9f0c051592e42
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-17 08:22:03 -0700

    Add files via upload

--- 15.py
+++ 15.py
@@ -1,15 +1,7 @@
-students = int(input("enter number of students"))
-hungry = bool(False)
-
-if students>= 90:
-    print('Today we make biryani')
-elif students>= 80:
-    if hungry== True:
-        print("today also make biryani")
-    else: 
-        print("today making uppito")
-
-elif students>=60:
-    print("today number of student less so we make rice and sambar")
-elif students>=50:
-    print("today we not make food so please eat outside")
+import datetime
+if datetime.datetime.now().hour < 12:
+    print("more buses expected at this time ")
+elif datetime.datetime.now().hour < 17:
+    print("less busses expected at this time")
+ 
+    
