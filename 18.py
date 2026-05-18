9fb0a78d608c34cdeb4785c5add05c4c87ad71fd
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-18 06:03:50 -0700

    Add files via upload

--- 18.py
+++ 18.py
@@ -1,6 +1,11 @@
-try:
-    file = open("sample.txt","r+")
-    data = file.read()
-    print(data)
-except:
-    print("file not found")
+n =int(input("enter the number of tuple element : "))
+
+tuple = ()
+for i in range(n):
+    element = int(input("enter the element: "))
+    tuple += (element,)
+print("the tuple is:", tuple)
+print("The smallest element in the tuple is:", min(tuple))
+print("The largest element in the tuple is:", max(tuple))
+print("The sum of all elements in the tuple is:", sum(tuple))
+print("The average of all elements in the tuple is:", sum(tuple)/len(tuple))
