e10df4300bdb030b2fe11d0c0b1291a2e23ea088
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-18 06:04:05 -0700

    Add files via upload

--- 19.py
+++ 19.py
@@ -1,32 +1,21 @@
-import math
-
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-def multiple(a,b):
-    return a*b
-def divide(a,b):
-    return a/b
-
-num = int(input("enter number"))
-a = int(input("enter first number"))
-b =int(input("enter second number"))
-
-
-if num == 1:
-    result = add(a,b)
-    print(result)
-elif num ==2:
-    result = sub(a,b)
-    print(result)
-elif num == 3:
-    result = multiple(a,b)
-    print(result)
-
-elif num == 4:
-    try:
-        result =  divide(a,b)
-        print(result)
-    except ZeroDivisionError:
-        print("we cannot divide by using zero")
+n = int(input("enter number items "))
+dict = {}
+for i in range(n):
+    key = input("enter the key: ")
+    value = input("enter the value: ")
+    dict[key] = value
+print("the dictionary is:", dict)
+print("the keys in the dictionary are:", dict.keys())
+print("the values in the dictionary are:", dict.values())
+print("the items in the dictionary are:", dict.items())
+key_to_remove = input("enter the key to remove: ")
+if key_to_remove in dict:
+    del dict[key_to_remove]
+    print("the dictionary after removing the key is:", dict)
+else:    print("the key is not found in the dictionary")
+key_to_update = input("enter the key to update: ")
+if key_to_update in dict:
+    new_value = input("enter the new value: ")
+    dict[key_to_update] = new_value
+    print("the dictionary after updating the key is:", dict)
+else:    print("the key is not found in the dictionary")
