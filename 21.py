398637640f8e23cda3bb52e962fd066267967426
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-19 08:16:57 -0700

    Add files via upload

--- 21.py
+++ 21.py
@@ -1,4 +1,10 @@
-N =int(input())
-
+N = int(input())
+l = []
 for i in range(N):
-    print(i*10**i//9)
+    val =input("enter number")
+    l.append(val)
+print(l)
+l.pop()
+l.append(5)
+l.append(12)
+print(l)
