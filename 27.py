4b8af9c8ad90fab5bc22d483e1b5e44f09b2532a
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-21 09:00:59 -0700

    Add files via upload

--- 27.py
+++ 27.py
@@ -1,29 +1,12 @@
-num = int(input("enter a number: "))
-match num:
-    case 1:
-        print("one")
-    case 2:
-        print("two")
-    case 3:
-        print("three")
-    case 4:
-        print("four")
-    case 5:
-        print("five")
-    case _:
-        print("number not in range")
-
-
-age = int(input("enter your age: "))
-
-match age:
-    case age if age <18:
-        print("you are a minor")
-    case age if age >= 18 and age < 25:
-        print("you are a young guy")
-    case age if age >= 25 and age < 60:
-        print("you are an adult")
-    case age if age >= 60 and age < 100:
-        print("you are a senior citizen")
-    case _:
-        print("invalid age")
+import calendar    #give the find the day of the week for any date
+m=int(input("Enter the month: "))
+d=int(input("Enter the day: "))
+y=int(input("Enter the year: "))
+day=calendar.weekday(y,m,d)
+if day==0:    print("Monday")
+elif day==1:    print("Tuesday")
+elif day==2:    print("Wednesday")
+elif day==3:    print("Thursday")
+elif day==4:    print("Friday")
+elif day==5:    print("Saturday")
+elif day==6:    print("Sunday")
