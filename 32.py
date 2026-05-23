048c47fc3475986cf3562a997db81defaba711e0
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-23 21:01:06 +0530

    Add files via upload

--- 32.py
+++ 32.py
@@ -1,24 +1,5 @@
-name  =  input("enter your name")
-roll_no = int(input("enter roll number"))
-subject = int(input("enter number of subjects"))
-total = 0
-for i in range(subject): 
-    subject_name =input(" enter name of subjec")
-    Marks = int(input("enter marks of subject"))
-    total = total+Marks
+import numpy as np
 
-print("Name:",name)
-print("Roll_no",roll_no)
-print("total marks:",total)
-if total>=90:
-    print("student get schlorship maximum")
-elif tptal>=80:
-    print("student eligible schlorship minimum")
-else:
-    print(" student difficult get schlorship")
-    
-
-
-    
-
-
+A = np.array([[1,2],[3,4]])
+B = np.array([[5,6],[7,8]])
+print(np.dot(A,B))
