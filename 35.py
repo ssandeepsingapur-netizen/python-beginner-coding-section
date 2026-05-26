ef1a570a55d456b1f5bf47948f5f8e3054739cd4
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-26 17:56:16 +0530

    Add files via upload

---
+++ 35.py
@@ -0,0 +1,13 @@
+def fun(a,b,c,d):
+    if a > b and a > c and a > d:
+        return a
+    elif b > a and b > c and b > d:
+        return b
+    elif c > a and c> b and c>d:
+        return c
+    else:
+        return d
+    
+
+print(fun(4,6,3,8))
+    
