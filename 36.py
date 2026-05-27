a9c9d6829ebbb11c5182b8365cc8eae3ab576615
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-27 22:11:47 +0530

    Add files via upload

--- 36.py
+++ 36.py
@@ -1,17 +1,14 @@
-import wikipedia as wiki
-def get_Wikipedia_summary(topic):
-    try:
-        summary = wiki.summary(topic)
-        return summary
-    except wiki.exceptions.DisambiguationError as e:
-        return f"Topic '{topic}' is ambiguous. Possible options: {e.options}"
-    except wiki.exceptions.PageError:
-        return f"Topic '{topic}' not found on Wikipedia."
-    except Exception as e:
-        return f"An error occurred: {str(e)}"
-    
+import os
+def show_files(folder):
+    files = os.listdir(folder)
+    for file in files:
+        path = os.path.join(folder,file)
+        if os.path.isdir(path):
+            print("Folder:",file)
+            show_files(path)
+        else:
+            print("file:",file)
 
-    # example usage
-topic = "virat kohli"
-summary = get_wikipedia_summary(topic)
-print(summary)
+
+folder_name = input("Enter the folder name:")
+show_files(folder_name)
