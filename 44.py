b754cb057ec253181a64345057b5cf408347ea73
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-31 12:20:44 +0530

    Add files via upload

---
+++ 44.py
@@ -0,0 +1,9 @@
+name = input("enter your name")
+year_of_birth = int(input("enter your year if birth"))
+current_year = 2026
+age = current_year- year_of_birth
+if age>=60:
+    print(name,"is a seior citizen")
+else:
+    print(name, "is not srnior citizen")
+    
