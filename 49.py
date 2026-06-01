7c33c85d2991625adf6d8e3aa4fe622cc5091991
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-06-01 23:14:36 +0530

    Add files via upload

---
+++ 49.py
@@ -0,0 +1,17 @@
+file = open("file.txt1","r")
+text = file.read()
+file.close()
+
+text = text.lower()
+words = text.split()
+freq = {}
+for word in words:
+    if word in freq:
+        freq[word]+= 1
+    else:
+        freq[word]=1
+
+sorted_freq = sorted(freq.items(),key=lambda x: x[1],reverse = True)
+print("Top 10 most frequenty words")
+for i in range(10):
+    print(f"{sorted_freq[i][0]}: {sorted_freq[i][1]}")
